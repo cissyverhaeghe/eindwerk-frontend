07537f00e6ecd38de6cf60f35883f4c88ffd6efc
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-06-05 14:41:00 +0200

    added the needed pages

---
+++ pages/cats.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const cats = () => {
+  return <div>cats</div>;
+};
+
+export default cats;
---
+++ pages/dogs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const dogs = () => {
+  return <div>dogs</div>;
+};
+
+export default dogs;
