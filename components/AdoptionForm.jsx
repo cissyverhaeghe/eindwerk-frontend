2d7203d23d80120405f473f4bbef64398e9b5834
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-06-15 14:52:17 +0200

    request form added

--- components/AdoptionForm.jsx
+++ components/AdoptionForm.jsx
@@ -4,49 +4,72 @@ import axios from "axios";
 
 const AdoptionForm = ({ animal: { id, name, photo } }) => {
   const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  console.log(submitted);
 
-  const handleFormSubmit = async () => {
-    axios
-      .post({
-        url: "http://127.0.0.1:8000/api/adoptionrequest",
-        data: {
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    (async () => {
+      try {
+        const body = {
           date: "2022-06-14T15:06:21.407Z",
-          message: "string",
-          animal: 1,
-          user: 1,
-          status: 1,
-        },
-      })
-      .then(
-        (response) => {
-          console.log(response);
-        },
-        (error) => {
-          console.log(error);
+          message: message,
+          animal_id: id,
+          user_id: 1,
+          status_id: 1,
+        };
+        const data = await axios("http://127.0.0.1:8000/api/adoptionrequest", {
+          method: "POST",
+          data: JSON.stringify(body),
+        });
+        console.log(data);
+        if (data) {
+          setSubmitted(true);
         }
-      );
+      } catch (error) {
+        console.log(error);
+      }
+    })();
   };
 
   return (
-    <div>
-      <h1>Adoption Request</h1>
-      <h2>You are filing a request to adopt {name}</h2>
-      <div className="imageholder">
-        <Image
-          src={"/" + photo}
-          alt="animal"
-          width={300}
-          height={300}
-          layout="responsive"
-        />
-      </div>
-      <h2>Please let us know why you would like to adopt {name}</h2>
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button onClick={handleFormSubmit}>Submit request</button>
+    <div className="adoption">
+      <section>
+        <h1>Adoption Request</h1>
+        <h2>You are filing a request to adopt {name}.</h2>
+
+        {!submitted && (
+          <>
+            <p>Please let us know why you would like to adopt {name}.</p>
+            <form>
+              <textarea
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Message"
+              />
+              <button onClick={handleFormSubmit}>Submit request</button>
+            </form>
+          </>
+        )}
+        {submitted && (
+          <>
+            <p>We have received your request successfully!</p>
+            <button>Go to overview</button>
+          </>
+        )}
+      </section>
+      <aside>
+        <div className="imageholder">
+          <Image
+            src={"/" + photo}
+            alt="animal"
+            width={300}
+            height={300}
+            layout="responsive"
+          />
+        </div>
+      </aside>
     </div>
   );
 };
